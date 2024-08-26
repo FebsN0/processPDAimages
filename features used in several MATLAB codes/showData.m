bf2cc7f82a4f97e96bc889a23981fcfbb8bf0abc
FebsN0 <fab.alt@protonmail.com>
2024-08-26 21:40:18 +0900

    added normalization plot

--- features used in several MATLAB codes/showData.m
+++ features used in several MATLAB codes/showData.m
@@ -1,13 +1,20 @@
-function showData(secondMonitorMain,SeeMe,i,data,titleData,labelBar,nameFig)
+function showData(secondMonitorMain,SeeMe,i,data,norm,titleData,labelBar,nameFig)
     if SeeMe
         eval(sprintf('f%d=figure(''Visible'',''on'');',i)) 
     else
         eval(sprintf('f%d=figure(''Visible'',''off'');',i)) 
     end
-    imagesc(data)
+
+    if norm
+        imshow(imadjust(data/max(max(data))))
+        c = colorbar; c.Label.String = 'normalized'; 
+    else
+        imagesc(data)
+        c = colorbar; c.Label.String = labelBar; 
+    end
     colormap parula, title(titleData,'FontSize',17),
-    c = colorbar; c.Label.String = labelBar; c.Label.FontSize=15;
-    ylabel('slow scan line direction','FontSize',12), xlabel('fast scan line direction','FontSize',12)
+    c.Label.FontSize=15;
+    xlabel('slow scan line direction','FontSize',12), ylabel('fast scan line direction','FontSize',12)
     axis equal, xlim([0 size(data,2)]), ylim([0 size(data,1)])
     if ~isempty(secondMonitorMain),objInSecondMonitor(secondMonitorMain,eval(sprintf('f%d',i))); end
     saveas(eval(sprintf('f%d',i)),nameFig)
