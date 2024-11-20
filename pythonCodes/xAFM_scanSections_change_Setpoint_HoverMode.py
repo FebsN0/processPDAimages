f64fa4a59a01670338fe37bf7ed73b3ecf21e2b0
FebsN0 <fab.alt@protonmail.com>
2024-11-20 18:55:37 +0900

    definitive version, improvement

--- pythonCodes/xAFM_scanSections_change_Setpoint_HoverMode.py
+++ pythonCodes/xAFM_scanSections_change_Setpoint_HoverMode.py
@@ -7,14 +7,11 @@ from com.jpk.spm.data import AmplitudeSetpointFeedbackSettings
 from com.jpk.data import SetpointFeedbackSettings
 from com.jpk.spm.data import HoverSpatialScanningStyle
 from com.jpk.spm.data import SimpleSpatialScanningStyle
-    
 from datetime import datetime
 
 '''
-Set vertical deflection setpoint for AC- Mode
-Please note: setpoint MUST be given in the current scaling
-of the setpoint (nm) or (V), depending on an active sensitivity
-calibration
+Set vertical deflection setpoint for AC-Mode. Depending on the activated calibration constant,
+convert the volt value into the specified unit (meter or newton)
 '''
 def setAmplitudeSetpoint(value):
     print('SET SETPOINT %e' % value)
@@ -37,7 +34,8 @@ def setAmplitudeSetpoint(value):
     return value
 
 '''
-Set vertical deflection setpoint for Contact-Mode
+Set vertical deflection setpoint for Contact-Mode. Depending on the activated calibration constant,
+convert the volt value into the specified unit (meter or newton)
 '''
 def setVDeflectionSetpoint(value):
     print('SET SETPOINT %.2e' % value)
@@ -45,20 +43,17 @@ def setVDeflectionSetpoint(value):
     feedbackModeInfo = instrument.getCurrentFeedbackModeInfo()
     feedbackSettings = feedbackModeInfo.getPrimaryFeedbackSettings()
     feedbackChannel = feedbackModeInfo.getFeedbackChannel()
-
     # scale setpoint value to base volts
     conversionSet = instrument.getChannelInfo(feedbackChannel).getConversionSet()
     calibrationSlot = conversionSet.getDefaultCalibrationSlot()
     setpointCalibrationSlot = feedbackSettings.getSetpointCalibrationSlot()
     setpointVolts = conversionSet.scale(value, calibrationSlot, setpointCalibrationSlot)
-
      #set new setpoint (changed here!)
     parameters = SetpointFeedbackSettings.Parameters(feedbackSettings)
     parameters.setRelativeSetpoint(setpointVolts)
     newFeedbackSettings = feedbackSettings.createModifiedSettings(parameters)
     feedbackModeInfo.setPrimaryFeedbackSettings(newFeedbackSettings)
     return value
-
 '''
 Enable/disable HoverMode
 '''
@@ -71,8 +66,6 @@ def enableHoverMode(enable):
         mode.setSpatialScanningStyle(HoverSpatialScanningStyle(style, True))
     else:
         mode.setSpatialScanningStyle(SimpleSpatialScanningStyle(style))
-
-
 '''
 Get the current vertical deflection baseline in the current vertical
 deflection unit, depending on the calibration
@@ -82,32 +75,26 @@ def getBaseline():
     feedbackModeInfo = instrument.getCurrentFeedbackModeInfo()
     feedbackSettings = feedbackModeInfo.getPrimaryFeedbackSettings()
     baselineVolts = feedbackSettings.getBaseline()
-
     feedbackChannel = feedbackModeInfo.getFeedbackChannel()
     conversionSet = instrument.getChannelInfo(feedbackChannel).getConversionSet()
     calibrationSlot = conversionSet.getDefaultCalibrationSlot()
-
     baseline = conversionSet.scale(baselineVolts, calibrationSlot)
-    print('GET BASELINE = %.2e' % baseline)
     return baseline
-
 '''
-Define a list of setpoints, here in VOLTS scaling. If you have calibrated 
-the sensitivity value, setpoint must be given in METER, 30 nm = 30e-9
+Define a list of setpoints, here in Newton scaling (sensitivity and spring constant activated)
 '''
-setpointList = [30e-9, 50e-9, 70e-9, 90e-9, 110e-9]
-springConstant= 0.452                                # N/m
-sensitivity=63.65
+setpointList = [50e-9, 50e-9, 50e-9,50e-9,50e-9]        # N
+springConstant= 0.435                       # N/m
+sensitivity=93.28                           # nm/V
 array = [0]         # 0: HOVER MODE ON      # 1: HOVER MODE OFF
 '''
 setup the base direcory for saving data
 this will be extended later in the loop to
 sort HoverMode ON/OFF scans
 '''
-baseDirName='/home/jpkuser/jpkdata/Fabiano/experiments/1_lipidEffects/3_3_1 TRCDA:DOPC/4'
+baseDirName='/home/jpkuser/jpkdata/Fabiano/experiments/1_lipidEffects/3_3_1 TRCDA:DOPC/afterheat/7'
 Imaging.setOutputDirectory(baseDirName+'/')
 Imaging.setAutosave(True)
-
 '''
 Setup the initial scan parameters at the current x,y offset position
 '''
@@ -123,18 +110,19 @@ scan_rate=      0.18    # Hz
 xOffset = Imaging.getXOffset()
 yOffset = Imaging.getYOffset()
 # save the origin of the y axis, so when the first entire scan is completed,
-# switch hover mode and come back to the y origin
+# come back to the y origin for the nee scan
 yOffset_origin = yOffset   
 
-Scanner.retractPiezo()
+#Scanner.retractPiezo()
 Imaging.setScanSize(scanSizeFast, scanSizeSlow)
 Imaging.setScanPixels(fastPixels, slowPixels)
 Imaging.setLineRate(scan_rate)
 
-# create textfile for metadata
-f = open(baseDirName+'/baseline.txt', 'w')
+# create textfile for metadata (if exist, append, to avoid overwrite in case of forgetting to change directory
+f = open(baseDirName+'/baseline.txt', 'a')
 # write start info and add a header
-f.write('# Start time = %s' % datetime.now())
+dateStart=datetime.now()
+f.write('\n# Start time = %s\n' % dateStart)
 f.write('# Scan output directory = %s\n' % baseDirName)
 f.write('# ScanSizeFast (X) = %e\n' % scanSizeFast)
 f.write('# ScanSizeSlow (Y) = %e\n' % scanSizeSlow)
@@ -158,38 +146,39 @@ try:
         for setpoint in setpointList:
             print('yOFFSET: %e' % yOffset)
             Imaging.setScanOffset(xOffset, yOffset)
-            
             # Set setpoint
-            #setAmplitudeSetpoint(setpoint)
             setVDeflectionSetpoint(setpoint)
             # approach and measure current baseline
             Scanner.approachPiezo()
             baselineStart = getBaseline()
             # start image scan
             Imaging.startScanning(1)
-            # after finishing the entire section scan, retract piezo and update baseline　at the
-            # end of the scan to compare how they changed
             Scanner.retractPiezo()
+
+            # approach again to update and save the baseline
             Scanner.approachPiezo()
-            baselineEnd = getBaseline() # not sure if it should be placed after approaching.. ###################
-            Scanner.retract()
+            baselineEnd = getBaseline()
+            Scanner.retractPiezo()
             
-            #increment scan index
+            #increment scan index 
             scanIndex += 1
             # write metadata to text file for each scan
             f.write('%d\t%e\t%e\t%e\t%e\t%e\n' % (scanIndex, setpoint, baselineStart, baselineEnd, xOffset, yOffset))
             f.flush()
-    
             # Move the scan in the slow scan direction by scanSizeSlow 
             yOffset += scanSizeSlow
     # once finished the entire scan for specific hover mode, let's back to the y offset origin 
     yOffset=yOffset_origin
     Imaging.setScanOffset(xOffset, yOffset)
-    
-    finally:
-        f.close()
+
+finally:
+    dateEnd=datetime.now()
+    f.write('\n# End time = %s\n' % dateEnd)
+    td= dateEnd - dateStart
+    duration_in_s = (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 10**6
+    duration_in_min = divmod(duration_in_s, 60)[0] 
+    f.write('# Total time (min)= %d\n' % duration_in_min)
+    f.close()
+    Scanner.moveMotorsUp(5e-3)
     
 print('DONE....')
-    
-    
-    
