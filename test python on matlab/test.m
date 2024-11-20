c7450234c65c5f1f61d65ef3fdc7636109674d03
FebsN0 <fab.alt@protonmail.com>
2024-11-20 15:24:40 +0900

    added small scripts to test working python on matlab

---
+++ test python on matlab/test.m
@@ -0,0 +1,35 @@
+% define MATLAB matrices
+A1=[1 2;
+    3 4];
+A2=[2 3;
+    1 0];
+ 
+% check the conversion types
+% https://www.mathworks.com/help/matlab/matlab_external/passing-data-to-python.html
+% convert matrices to Python objects
+A1converted = py.numpy.array(A1);
+A2converted = py.numpy.array(A2);
+% parameter to be sent
+parameter=py.int(2);
+
+% sent the variables, call the python code in "test.py" and obtain the
+% return list "result"
+[result] = pyrunfile("test.py","ReturnList",A=A1converted,B=A2converted,param1=parameter);
+% link explaining pyrunfile() function:
+% https://www.mathworks.com/help/matlab/ref/pyrunfile.html#mw_03ecec06-0677-4345-9112-ea93ac49881e
+
+% check the data type of returned variables
+class(result)
+class(result{1})
+
+% convert the Python arrays to MATLAB matrices
+C1=double(result{1});
+C2=double(result{2});
+C3=double(result{3});
+C4=double(result{4});
+
+% compute the matrices in MATLAB to double check the results
+C1check=A1+A2;
+C2check=A1*A2;
+C3check=eye(2,2);
+C4check=inv(A1);
---
+++ test python on matlab/test.py
@@ -0,0 +1,10 @@
+import numpy as np
+from scipy import linalg
+ 
+C1=A+B
+C2=np.matmul(A,B)
+C3=np.eye(param1,param1)
+C4=linalg.inv(A)
+ 
+# this list is returned to MATLAB and later on unpacked in MATLAB
+ReturnList=[C1,C2,C3,C4]
