b6a7c40ebecf3503ecf9c2b4924fd3036384e378
FebsN0 <fab.alt@protonmail.com>
2024-11-20 15:42:00 +0900

    version 3 code improvements

--- pythonCodes/xAFM_scanSections_change_Setpoint_HoverMode.py
+++ pythonCodes/xAFM_scanSections_change_Setpoint_HoverMode.py
@@ -1,6 +1,6 @@
 # Python code for ExperimentPlanner
 # JPK Script
-checkVersion('SPM', 6, 1, 186)
+checkVersion('SPM', 6, 4, 32)
 
 from com.jpk.inst.lib import JPKScript
 from com.jpk.spm.data import AmplitudeSetpointFeedbackSettings
@@ -52,30 +52,29 @@ the sensitivity value, setpoint must be given in METER, 30 nm = 30e-9
 Otherwise, enter the sensitivity and spring constant. Conver the nN into Volts
 '''
 
-setpointListNanoNewton = [20, 40, 60, 80, 100]   # nN
+setpointListNanoNewton = [30, 50, 70, 90, 110]   # nN
 setpointListNewton=[x * 1e-9 for x in setpointListNanoNewton]   # N
-springConstant= 0.264                                 # N/m
-sensitivity=80.60                                       # nm/V
+springConstant= 0.452                                # N/m
+sensitivity=63.65                              # nm/V
 setpointListVolts=[x / (springConstant*sensitivity) for x in setpointListNanoNewton]
 
-'''
-setup the base directory for saving data
-this will be extended later in the loop to
-sort HoverMode ON/OFF scans
-'''
 
-baseDirName='/home/jpkuser/jpkdata/Fabiano/experiments/1_lipidEffects/2_2_TRCDA_DMPC_7525_10mM_22luglio2024/4_air_03Hz'
+'''
+Setup the parameters: initial scan size of a single section at the current x,y offset position, pixel size, scan line rate.
+'''
+array = [0]         # 0: HOVER MODE ON      # 1: HOVER MODE OFF
+# setup the base directory for saving data this will be extended later in the loop to sort HoverMode ON/OFF scans
+baseDirName='/home/jpkuser/jpkdata/Fabiano/experiments/1_lipidEffects/3_3_1 TRCDA:DOPC/4'
+
+# if 10 setpoints  ==> 10 slow direction scan sections with a size of 5 um if fast scan line direction is 50 um long
+scanSizeFast =  80.0e-6     # 50 um
+scanSizeSlow =  scanSizeFast / len(setpointListVolts)          
+fastPixels=     1024           # 640 for 5 sections ==> 140*5 = 640 pixels
+slowPixels=     fastPixels/len(setpointListVolts)
+scan_rate=      0.18    # Hz
+
 Imaging.setOutputDirectory(baseDirName)
 Imaging.setAutosave(True)
-
-
-'''
-Setup the initial scan size of a single section at the current x,y offset position.
-'''
-scanSizeFast = 50.0e-6     # 50 um
-# if 10 setpoints  ==> 10 slow direction scan sections with a size of 5 um if fast scan line direction is 50 um long
-scanSizeSlow = scanSizeFast / len(setpointListVolts)
-
 # extract the origin of scanning
 xOffset = Imaging.getXOffset()
 yOffset = Imaging.getYOffset()
@@ -83,14 +82,7 @@ yOffset = Imaging.getYOffset()
 yOffset_origin = yOffset                             
 Scanner.retractPiezo()
 Imaging.setScanSize(scanSizeFast, scanSizeSlow)
-
-'''
-Set other parameters (pixel size, scan line rate)
-'''
-fastPixels=600			# 512 for 8 sections ==> 512+64+64 = 640 pixels
-slowPixels= fastPixels/len(setpointListVolts)
 Imaging.setScanPixels(fastPixels, slowPixels)
-scan_rate=0.5	# 1 Hz
 Imaging.setLineRate(scan_rate)
 
 '''
@@ -105,22 +97,25 @@ conditionHVmode = [True, False]
 # two for cycles:
 # Cycle 1 HoverMode ON
 # Cycle 2 HoverMode OFF 
-for i in range(0,1):
+for i in array:
     Imaging.setOutputDirectory(baseDirName+textDirectory[i])
     enableHoverMode(conditionHVmode[i])
 
+    #Scanner.approach()
     for setpoint in setpointListVolts:
-        print(yOffset)
+        print('yOFFSET: %e' % yOffset)
         Imaging.setScanOffset(xOffset, yOffset)
         # Set setpoint
         setAmplitudeSetpoint(setpoint)    
         Scanner.approachPiezo()
         Imaging.startScanning(1)
-        Scanner.retractPiezo()    
+        Scanner.retractPiezo()
+            
         # Move the scan in the slow scan direction by scanSizeSlow 
         yOffset += scanSizeSlow
-
-    
+    Scanner.retract()
     yOffset=yOffset_origin
     Imaging.setScanOffset(xOffset, yOffset)
+
 print('DONE....')
+
