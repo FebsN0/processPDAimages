03cb21470557f98d8a6eef9c936d1c53e7587910
FebsN0 <fab.alt@protonmail.com>
2025-02-17 22:17:39 +0900

    added globalColor

---
+++ features used in several MATLAB codes/globalColor.m
@@ -0,0 +1,4 @@
+function col = globalColor(n)
+    colors={"#0072BD","#D95319","#EDB120","#7E2F8E","#77AC30","#4DBEEE","#A2142F",'k','#FF00FF','#00FF00','#0000FF','#FF0000'};
+    col=colors{n};
+end
